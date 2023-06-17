c33b1f9bda9edd5e4a5ff188ae28cb18a7051865
Sam <60146956+srudqvist@users.noreply.github.com>
2023-06-17 12:43:23 -0500

    Changed font and added particles

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
+import ParticlesBg from "particles-bg";
 
 function App() {
 	return (
 		<div className="App">
+			<ParticlesBg type="cobweb" bg={true} num={100} color="#FFFFFF" />
 			<Navigation />
 
 			<Logo />
